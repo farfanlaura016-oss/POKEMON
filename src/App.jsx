67025d05f97250e2eb43b56b453bbebac0eec661
Laura Sofya Farfan Muñoz <farfanlaura016@gmail.com>
2026-09-08 09:05:31 -0500

    Se realizaron todas las actualizaciones, consumir PokeAPI con GET, configurar API local con JSON Server, implementar CRUD del equipo Pokemon

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import Pokedex from "./pages/Pokedex";
+import MiEquipo from "./pages/MiEquipo";
+
+import "./index.css";
+
+function App() {
+
+    const [actualizarEquipo, setActualizarEquipo] =
+        useState(0);
+
+    return (
+
+        <main>
+
+            <h1>
+                Pokédex React
+            </h1>
+
+            <Pokedex
+                onPokemonAgregado={() =>
+                    setActualizarEquipo(
+                        valor => valor + 1
+                    )
+                }
+            />
+
+            <hr />
+
+            <MiEquipo
+                actualizarEquipo={actualizarEquipo}
+            />
+
+        </main>
+
+    );
+}
+
+export default App;
---
+++ src/services/equipoApi.js
@@ -0,0 +1,60 @@
+const API = "http://localhost:3001/equipo";
+
+export const obtenerEquipo = async () => {
+    const respuesta = await fetch(API);
+
+    if (!respuesta.ok) {
+        throw new Error("No fue posible cargar el equipo");
+    }
+
+    return await respuesta.json();
+};
+
+export const agregarAlEquipo = async (pokemon) => {
+    const respuesta = await fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pokemon)
+    });
+
+    if (!respuesta.ok) {
+        throw new Error("No fue posible agregar el Pokémon");
+    }
+
+    return await respuesta.json();
+};
+
+export const actualizarPokemon = async (id, cambios) => {
+    const respuesta = await fetch(
+        `${API}/${id}`,
+        {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(cambios)
+        }
+    );
+
+    if (!respuesta.ok) {
+        throw new Error("No se pudo actualizar");
+    }
+
+    return await respuesta.json();
+};
+export const eliminarPokemon = async (id) => {
+    const respuesta = await fetch(
+        `${API}/${id}`,
+        {
+            method: "DELETE"
+        }
+    );
+
+    if (!respuesta.ok) {
+        throw new Error(
+            "No se pudo eliminar el Pokémon"
+        );
+    }
+};
